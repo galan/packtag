ad309b7497704d82c758fb756c8e503ed6ae7c17
Daniel Galán y Martins <git@galan.de>
2008-11-23 10:13:33 +0000

    name

--- packtag-core/src/main/java/net/sf/packtag/strategy/PackStrategy.java
+++ packtag-core/src/main/java/net/sf/packtag/strategy/PackStrategy.java
@@ -28,7 +28,7 @@ import java.nio.charset.Charset;
  * A Strategy has to implement this Interface, the algorithm needs to return the packed
  * resource. 
  * 
- * @author Daniel Gal·n y Martins
+ * @author Daniel Gal√°n y Martins
  * @version $Revision: 1.2 $
  */
 public interface PackStrategy {
